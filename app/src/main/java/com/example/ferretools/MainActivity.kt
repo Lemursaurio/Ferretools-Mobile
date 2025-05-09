4a60343e6bfbab4a7e9714fab6c87d3db1c1c6ec
HerberthAsencios <herberth.asencios@unmsm.edu.pe>
2025-05-09 14:53:04 -0500

    inventario, crear producto, crear intario.

--- app/src/main/java/com/example/ferretools/MainActivity.kt
+++ app/src/main/java/com/example/ferretools/MainActivity.kt
@@ -5,35 +5,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ferretools.ui.theme.FerreToolsTheme
+import androidx.navigation.compose.rememberNavController
+import com.example.ferretools.ui.navigation.NavGraph
+import com.example.ferretools.ui.theme.FerretoolsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FerreToolsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "FerreTools",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    FerretoolsTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            val navController = rememberNavController()
+            NavGraph(navController = navController)
+        }
+    }
 }
+
+
